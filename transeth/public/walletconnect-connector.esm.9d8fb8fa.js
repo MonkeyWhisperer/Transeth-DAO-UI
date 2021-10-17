f2d0d922fa17c572536c9479ca0ea2d373474cb7
Rusan Alexandru <rusannalexandru@gmail.com>
2021-10-17 19:01:36 +0300

    UI

--- transeth/public/walletconnect-connector.esm.9d8fb8fa.js
+++ transeth/public/walletconnect-connector.esm.9d8fb8fa.js
@@ -460,7 +460,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50882" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53460" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
